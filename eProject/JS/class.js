bacd0836d07c99493857d9dadf2dada3f7839dad
Chubbyboiz <nguyenphuong010109@gmail.com>
2023-09-28 16:40:24 +0700

    Demo

---
+++ eProject/JS/class.js
@@ -0,0 +1,14 @@
+var offlineClass = [
+    {title: "Modern Calligraphy Class + Kit Bundle", price:"$152.00", img:"Modern Calligraphy Class + Kit Bundle.jpg"},
+    {title: "Brush Calligraphy Class + Kit Bundle", price:"$155.00", img:"Brush Calligraphy Class + Kit Bundle.jpg"},
+    {title: "VIRTUAL Beginner Journaling Workshop", price:"$155.00", img:"VIRTUAL Beginner Journaling Workshop.png"},
+]
+
+var onlineClass = [
+    {title: "Brush Calligraphy Class", price:"$115.00", img:"mitsubishi_xpander.png"},
+    {title: "Chalk Lettering with Skyler Chubak", price:"$115.00", img:"CRV-2020.png"},
+    {title: "Hand-Lettering Foundations Mini Class", price:"$50.00", img:"CRV-2020.png"},
+    {title: "Modern Calligraphy Class", price:"$115.00", img:"CRV-2020.png"},
+    {title: "Spencerian with Hoang Dao", price:"$150.00", img:"CRV-2020.png"},
+    {title: "Custom Calligraphy Vinyl Die Cut Name Sticker", price:"$15.00", img:"CRV-2020.png"},
+]
